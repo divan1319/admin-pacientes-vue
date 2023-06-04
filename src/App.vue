d3109e1fc91af6d066dde705270591cac5d6b755
Daniel Iván López García <lopez.garcia1319@gmail.com>
2023-06-03 22:41:46 -0600

    uid integrado

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@ import { reactive, ref } from "vue";
 import Formulario from "./components/Formulario.vue";
 import Header from "./components/Header.vue";
 import Paciente from "./components/Paciente.vue";
+import { uid } from "uid";
 const pacientes = ref([]);
 const paciente = reactive({
+  id: null,
   nombre: "",
   propietario: "",
   email: "",
@@ -15,6 +17,7 @@ const paciente = reactive({
 const guardarPaciente = () => {
   pacientes.value.push({
     ...paciente,
+    id: uid(),
   });
 
   Object.assign(paciente, {
@@ -26,9 +29,10 @@ const guardarPaciente = () => {
   });
 };
 
-const editarPaciente = ()=>{
-  
-}
+const editarPaciente = (id) => {
+  const pacienteEditar = pacientes.value.filter(p => p.id === id)[0]
+  Object.assign(paciente,pacienteEditar)
+};
 </script>
 
 <template>
@@ -41,12 +45,20 @@ const editarPaciente = ()=>{
         v-model:email="paciente.email"
         v-model:alta="paciente.alta"
         v-model:sintomas="paciente.sintomas"
-        @guardar-paciente="guardarPaciente"
-      />
+        @guardar-paciente="guardarPaciente" />
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
-        <h3 class="font-black text-3xl text-center">Pacientes en cola</h3>
+        <h2 class="font-black text-3xl text-center">
+          Ingreso de pacientes
+        </h2>
+        <p class="text-lg mt-5 text-center mb-10">
+          Mascotas ingresadas
+          <span class="text-indigo-600 font-bold">Administrar</span>
+        </p>
         <div v-if="pacientes.length > 0">
-          <Paciente v-for="paciente in pacientes" :paciente="paciente" />
+          <Paciente
+            v-for="paciente in pacientes"
+            :paciente="paciente"
+            @actualizar-paciente="editarPaciente" />
         </div>
         <p v-else class="mt-10 text-base font-bold text-center">
           No hay pacientes
--- src/components/Paciente.vue
+++ src/components/Paciente.vue
@@ -5,12 +5,14 @@ const props = defineProps({
         required:true
     }
 })
+
+defineEmits(['actualizar-paciente'])
 </script>
 <template>
 <div class="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
         <p class="font-bold mb-3 text-gray-700 uppercase">ID:
             <span class="font-normal normal-case">
-            
+            {{ paciente.id }}
             </span>
         </p>
 
@@ -52,6 +54,7 @@ const props = defineProps({
             <button 
                 type="button"
                 class="block w-full py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                @click="$emit('actualizar-paciente',paciente.id)"
             >Editar</button>
 
             <button 
